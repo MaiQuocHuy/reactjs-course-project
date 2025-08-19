19bc63649b05f0539f43c85ac4457d87ddd5cd9c
Phuong <phuong9agt@gmail.com>
2025-08-19 15:38:21 +0700

    feat: implement user management functionality with API integration for fetching, updating, and filtering users

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,16 +3,21 @@ import counterSlice from "@/features/counter/counterSlice";
 import paymentsSlice from "@/features/payments/paymentsSlice";
 import refundsSlice from "@/features/refunds/refundsSlice";
 import { authApi } from "@/services/authApi";
+import { usersApi } from "@/services/usersApi";
 import { configureStore } from "@reduxjs/toolkit";
+
 export const store = configureStore({
   reducer: {
     [authApi.reducerPath]: authApi.reducer,
+    [usersApi.reducerPath]: usersApi.reducer,
     counter: counterSlice.reducer,
     payments: paymentsSlice.reducer,
     refunds: refundsSlice,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authApi.middleware),
+    getDefaultMiddleware()
+      .concat(authApi.middleware)
+      .concat(usersApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
