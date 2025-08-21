665506b307747cc60da2d687af3f138aeb3823ab
Phuong <phuong9agt@gmail.com>
2025-08-21 14:54:22 +0700

    refactor: clean up imports and remove duplicate middleware in store configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,23 @@
-import './App.css';
+import "./App.css";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Navigate,
-} from 'react-router-dom';
-import LoginForm from './components/LoginForm';
-import { AdminLayout } from './components/admin/AdminLayout';
-import { DashboardPage } from './pages/admin/DashboardPage';
-import { UsersListPage } from './pages/admin/users';
-import { UserDetailPage } from './pages/admin/users';
-import { PaymentsPage } from './pages/payments';
-import RefundsPage from './pages/refunds';
-import RevenuesPage from './pages/revenues/RevenuesPage';
-import CoursesPage from './pages/courses/CoursesPage';
-import CourseDetailPage from './pages/courses/course-detail/CourseDetailPage';
-import CourseReviewDetailPage from './pages/courses/course-detail/CourseReviewDetailPage';
 } from "react-router-dom";
-import { Toaster } from "@/components/ui/sonner";
 import LoginForm from "./components/LoginForm";
 import { AdminLayout } from "./components/admin/AdminLayout";
 import { DashboardPage } from "./pages/admin/DashboardPage";
 import { UsersListPage } from "./pages/admin/users";
 import { UserDetailPage } from "./pages/admin/users";
-import { CategoriesListPage } from "./pages/admin/categories/CategoriesListPage";
 import { PaymentsPage } from "./pages/payments";
 import RefundsPage from "./pages/refunds";
+import RevenuesPage from "./pages/revenues/RevenuesPage";
+import CoursesPage from "./pages/courses/CoursesPage";
+import CourseDetailPage from "./pages/courses/course-detail/CourseDetailPage";
+import CourseReviewDetailPage from "./pages/courses/course-detail/CourseReviewDetailPage";
+import { Toaster } from "@/components/ui/sonner";
+import { CategoriesListPage } from "./pages/admin/categories/CategoriesListPage";
 
 function App() {
   return (
